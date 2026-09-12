6c1958eb73d729b8f8b8bdb8b8b0102f1a6b5c79
Tung3TIMES <pplazath@gmail.com>
2026-09-12 14:21:15 +0700

    SampleScene Load Exit

---
+++ Assets/Script/Loading.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+
+    [SerializeField]
+    private float waitsecond = 1f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitsecond > 0f) 
+            waitsecond -= Time.deltaTime;
+        else
+            StartCoroutine(LoadNewScene()); 
+    }
+
+    private IEnumerator LoadNewScene()
+    {
+        AsyncOperation oper = SceneManager.LoadSceneAsync("SampleScene");
+
+        while(!oper.isDone)
+        {
+            slider.value = oper.progress / 0.9f;
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("Loading");
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
